2039070984c0c09e0e291a8b9e9e6fd2ae033277
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-19 22:19:00 +0700

    add login admin or users

---
+++ client/src/app/api/auth/login/route.ts
@@ -0,0 +1,45 @@
+import authApiRequest from "@/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+export async function POST(request: Request) {
+  const body = (await request.json()) as LoginBodyType;
+  const cookieStore = cookies();
+  try {
+    const { payload } = await authApiRequest.sLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+    const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+    (await cookieStore).set("access_token", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedAccessToken.exp * 1000,
+    });
+    (await cookieStore).set("refresh_token", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, {
+        status: error.status,
+      });
+    } else {
+      return Response.json(
+        {
+          message: "có lỗi xảy ra",
+        },
+        {
+          status: 500,
+        }
+      );
+    }
+  }
+}
