f6f063d6ce5e1f95e91af48bd5a18956e5d878cc
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-25 13:18:10 +0700

    update logout handler

---
+++ client/src/app/api/auth/logout/route.ts
@@ -0,0 +1,35 @@
+import authApiRequest from "@/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+export async function POST(request: Request) {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  cookieStore.delete("accessToken");
+  cookieStore.delete("refreshToken");
+
+  if (!accessToken || !refreshToken) {
+    return Response.json(
+      {
+        message: "Không nhận được access token hoặc refresh token",
+      },
+      {
+        status: 200,
+      }
+    );
+  }
+  try {
+    const result = await authApiRequest.sLogout({
+      accessToken,
+      refreshToken,
+    });
+    return Response.json(result.payload);
+  } catch (error) {
+    return Response.json("Lỗi khi gọi api đến server backend", {
+      status: 200,
+    });
+  }
+}
