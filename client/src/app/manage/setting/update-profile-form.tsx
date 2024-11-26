0b59aff16b8a6a827d0d2a67037c1f692ad19f21
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-26 14:31:02 +0700

    fix upload profile

--- client/src/app/manage/setting/update-profile-form.tsx
+++ client/src/app/manage/setting/update-profile-form.tsx
@@ -1,50 +1,98 @@
-'use client'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { Upload } from 'lucide-react'
-import { useForm } from 'react-hook-form'
-import { UpdateMeBody, UpdateMeBodyType } from '@/schemaValidations/account.schema'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Upload } from "lucide-react";
+import { useForm } from "react-hook-form";
+import {
+  UpdateMeBody,
+  UpdateMeBodyType,
+} from "@/schemaValidations/account.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { useAccountProfile } from "@/queries/useAccount";
 
 export default function UpdateProfileForm() {
+  const [file, setFile] = useState<File | null>(null);
+  const avatarInputRef = useRef<HTMLInputElement>(null);
   const form = useForm<UpdateMeBodyType>({
     resolver: zodResolver(UpdateMeBody),
     defaultValues: {
-      name: '',
-      avatar: ''
-    }
-  })
+      name: "",
+      avatar: "",
+    },
+  });
+  const { data } = useAccountProfile();
 
+  const avatar = form.watch("avatar");
+  const name = form.watch("name");
+  useEffect(() => {
+    if (data) {
+      {
+        const { name, avatar } = data?.payload.data;
+        console.log(name, avatar);
+
+        form.reset({
+          name,
+          avatar: avatar ?? "",
+        });
+      }
+    }
+  }, [data, form]);
+  // Nếu dùng next 15 (React 19) => ko cần dùng memo
+  const previewAvatar = useMemo(() => {
+    if (file) {
+      return URL.createObjectURL(file);
+    }
+    return avatar;
+  }, [file, avatar]);
   return (
     <Form {...form}>
-      <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8'>
-        <Card x-chunk='dashboard-07-chunk-0'>
+      <form
+        noValidate
+        className="grid auto-rows-max items-start gap-4 md:gap-8"
+      >
+        <Card x-chunk="dashboard-07-chunk-0">
           <CardHeader>
             <CardTitle>Thông tin cá nhân</CardTitle>
           </CardHeader>
           <CardContent>
-            <div className='grid gap-6'>
+            <div className="grid gap-6">
               <FormField
                 control={form.control}
-                name='avatar'
+                name="avatar"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='flex gap-2 items-start justify-start'>
-                      <Avatar className='aspect-square w-[100px] h-[100px] rounded-md object-cover'>
-                        <AvatarImage src={'Duoc'} />
-                        <AvatarFallback className='rounded-none'>{'duoc'}</AvatarFallback>
+                    <div className="flex gap-2 items-start justify-start">
+                      <Avatar className="aspect-square w-[100px] h-[100px] rounded-md object-cover">
+                        <AvatarImage src={previewAvatar} />
+                        <AvatarFallback className="rounded-none">
+                          {name}
+                        </AvatarFallback>
                       </Avatar>
-                      <input type='file' accept='image/*' className='hidden' />
+                      <input
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        ref={avatarInputRef}
+                        onChange={(e) => {
+                          const file = e.target.files?.[0];
+                          console.log(file);
+                          if (file) {
+                            setFile(file);
+                          }
+                        }}
+                      />
                       <button
-                        className='flex aspect-square w-[100px] items-center justify-center rounded-md border border-dashed'
-                        type='button'
+                        className="flex aspect-square w-[100px] items-center justify-center rounded-md border border-dashed"
+                        type="button"
+                        onClick={() => avatarInputRef.current?.click()}
                       >
-                        <Upload className='h-4 w-4 text-muted-foreground' />
-                        <span className='sr-only'>Upload</span>
+                        <Upload className="h-4 w-4 text-muted-foreground" />
+                        <span className="sr-only">Upload</span>
                       </button>
                     </div>
                   </FormItem>
@@ -53,23 +101,28 @@ export default function UpdateProfileForm() {
 
               <FormField
                 control={form.control}
-                name='name'
+                name="name"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid gap-3'>
-                      <Label htmlFor='name'>Tên</Label>
-                      <Input id='name' type='text' className='w-full' {...field} />
+                    <div className="grid gap-3">
+                      <Label htmlFor="name">Tên</Label>
+                      <Input
+                        id="name"
+                        type="text"
+                        className="w-full"
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
                 )}
               />
 
-              <div className=' items-center gap-2 md:ml-auto flex'>
-                <Button variant='outline' size='sm' type='reset'>
+              <div className=" items-center gap-2 md:ml-auto flex">
+                <Button variant="outline" size="sm" type="reset">
                   Hủy
                 </Button>
-                <Button size='sm' type='submit'>
+                <Button size="sm" type="submit">
                   Lưu thông tin
                 </Button>
               </div>
@@ -78,5 +131,5 @@ export default function UpdateProfileForm() {
         </Card>
       </form>
     </Form>
-  )
+  );
 }
--- client/src/queries/useAccount.tsx
+++ client/src/queries/useAccount.tsx
@@ -5,7 +5,5 @@ export const useAccountProfile = () => {
   return useQuery({
     queryKey: ["accountProfile"],
     queryFn: () => accountApiRequest.me(),
-    staleTime: 1000 * 60 * 5, // Cache for 5 minutes
-    refetchInterval: 1000 * 60 * 5, // Refresh every 5 minutes
   });
 };
