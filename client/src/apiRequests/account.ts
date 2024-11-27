c0578eb419c6cdfd365ca96352674552ba27e8e9
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-27 13:22:07 +0700

    update change password

--- client/src/apiRequests/account.ts
+++ client/src/apiRequests/account.ts
@@ -1,6 +1,7 @@
 import http from "@/lib/http";
 import {
   AccountResType,
+  ChangePasswordBodyType,
   UpdateMeBodyType,
 } from "@/schemaValidations/account.schema";
 
@@ -8,6 +9,8 @@ const accountApiRequest = {
   me: () => http.get<AccountResType>("/accounts/me"),
   updateMe: (body: UpdateMeBodyType) =>
     http.put<AccountResType>("/accounts/me", body),
+  changePassword: (body: ChangePasswordBodyType) =>
+    http.put<AccountResType>("/accounts/change-password", body),
 };
 
 export default accountApiRequest;
--- client/src/app/manage/setting/change-password-form.tsx
+++ client/src/app/manage/setting/change-password-form.tsx
@@ -1,42 +1,70 @@
-'use client'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
-import { useForm } from 'react-hook-form'
-import { ChangePasswordBody, ChangePasswordBodyType } from '@/schemaValidations/account.schema'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { useForm } from "react-hook-form";
+import {
+  ChangePasswordBody,
+  ChangePasswordBodyType,
+} from "@/schemaValidations/account.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { useChangePasswordMutation } from "@/queries/useAccount";
+import { useState } from "react";
 
 export default function ChangePasswordForm() {
+  const [messages, setMessages] = useState<string>();
   const form = useForm<ChangePasswordBodyType>({
     resolver: zodResolver(ChangePasswordBody),
     defaultValues: {
-      oldPassword: '',
-      password: '',
-      confirmPassword: ''
+      oldPassword: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+  const changePasswordMutation = useChangePasswordMutation();
+  const onReset = () => {
+    form.reset();
+  };
+  const onSubmit = async (data: any) => {
+    try {
+      const result = await changePasswordMutation.mutateAsync(data);
+      setMessages(result.payload.message);
+      onReset();
+    } catch (error) {
+      setMessages(error as string);
     }
-  })
-
+  };
   return (
     <Form {...form}>
-      <form noValidate className='grid auto-rows-max items-start gap-4 md:gap-8'>
-        <Card className='overflow-hidden' x-chunk='dashboard-07-chunk-4'>
+      <form
+        onReset={onReset}
+        onSubmit={form.handleSubmit(onSubmit)}
+        noValidate
+        className="grid auto-rows-max items-start gap-4 md:gap-8"
+      >
+        <Card className="overflow-hidden" x-chunk="dashboard-07-chunk-4">
           <CardHeader>
             <CardTitle>Đổi mật khẩu</CardTitle>
             {/* <CardDescription>Lipsum dolor sit amet, consectetur adipiscing elit</CardDescription> */}
           </CardHeader>
           <CardContent>
-            <div className='grid gap-6'>
+            <div className="grid gap-6">
               <FormField
                 control={form.control}
-                name='oldPassword'
+                name="oldPassword"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid gap-3'>
-                      <Label htmlFor='oldPassword'>Mật khẩu cũ</Label>
-                      <Input id='oldPassword' type='password' className='w-full' {...field} />
+                    <div className="grid gap-3">
+                      <Label htmlFor="oldPassword">Mật khẩu cũ</Label>
+                      <Input
+                        id="oldPassword"
+                        type="password"
+                        className="w-full"
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
@@ -44,12 +72,17 @@ export default function ChangePasswordForm() {
               />
               <FormField
                 control={form.control}
-                name='password'
+                name="password"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid gap-3'>
-                      <Label htmlFor='password'>Mật khẩu mới</Label>
-                      <Input id='password' type='password' className='w-full' {...field} />
+                    <div className="grid gap-3">
+                      <Label htmlFor="password">Mật khẩu mới</Label>
+                      <Input
+                        id="password"
+                        type="password"
+                        className="w-full"
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
@@ -57,27 +90,39 @@ export default function ChangePasswordForm() {
               />
               <FormField
                 control={form.control}
-                name='confirmPassword'
+                name="confirmPassword"
                 render={({ field }) => (
                   <FormItem>
-                    <div className='grid gap-3'>
-                      <Label htmlFor='confirmPassword'>Nhập lại mật khẩu mới</Label>
-                      <Input id='confirmPassword' type='password' className='w-full' {...field} />
+                    <div className="grid gap-3">
+                      <Label htmlFor="confirmPassword">
+                        Nhập lại mật khẩu mới
+                      </Label>
+                      <Input
+                        id="confirmPassword"
+                        type="password"
+                        className="w-full"
+                        {...field}
+                      />
                       <FormMessage />
                     </div>
                   </FormItem>
                 )}
               />
-              <div className=' items-center gap-2 md:ml-auto flex'>
-                <Button variant='outline' size='sm'>
+              <div className=" items-center gap-2 md:ml-auto flex">
+                <Button variant="outline" size="sm">
                   Hủy
                 </Button>
-                <Button size='sm'>Lưu thông tin</Button>
+                <Button size="sm">Lưu thông tin</Button>
               </div>
+              {messages && (
+                <div className="text-center text-white bg-blue-950 rounded-sm shadow-sm font-semibold text-sm p-1 mt-3">
+                  {messages}
+                </div>
+              )}
             </div>
           </CardContent>
         </Card>
       </form>
     </Form>
-  )
+  );
 }
