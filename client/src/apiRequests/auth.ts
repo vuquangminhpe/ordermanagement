32b52d7a11cda09d7f42a707e83f7571c24a888c
minhminh <minhvqhe176726@fpt.edu.vn>
2024-11-29 13:35:18 +0700

    update refreshToken routes

--- client/src/apiRequests/auth.ts
+++ client/src/apiRequests/auth.ts
@@ -3,6 +3,8 @@ import {
   LoginBodyType,
   LoginResType,
   LogoutBodyType,
+  RefreshTokenBodyType,
+  RefreshTokenResType,
 } from "@/schemaValidations/auth.schema";
 
 const authApiRequest = {
@@ -22,5 +24,11 @@ const authApiRequest = {
       }
     ),
   logout: () => http.post("/api/auth/logout", null, { baseUrl: "" }),
+  sRefreshToken: (body: RefreshTokenBodyType) =>
+    http.post<RefreshTokenResType>("/auth/refresh-token", body),
+  refreshToken: () =>
+    http.post<RefreshTokenResType>("/api/auth/refresh-token", null, {
+      baseUrl: "",
+    }),
 };
 export default authApiRequest;
